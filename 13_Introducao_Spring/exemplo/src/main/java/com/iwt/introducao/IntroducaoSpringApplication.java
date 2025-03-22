6af4baf845875ddeb6458f8ff94cbfd223caa9a8
Kizzyan <yan.leitao26@gmail.com>
2025-03-22 12:50:55 -0300

    Finalização da aula 13: Introdução a Spring

---
+++ 13_Introducao_Spring/exemplo/src/main/java/com/iwt/introducao/IntroducaoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.iwt.introducao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoSpringApplication.class, args);
+	}
+
+}
